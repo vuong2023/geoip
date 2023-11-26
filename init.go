c48f2389e5b76cdef15dcb76d0fd360e4bb0f2a1
vuong2023 <124447749+vuong2023@users.noreply.github.com>
2023-11-26 12:54:28 +0700

    Update init.go

--- init.go
+++ init.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	_ "github.com/Loyalsoldier/geoip/plugin/maxmind"
-	_ "github.com/Loyalsoldier/geoip/plugin/plaintext"
-	_ "github.com/Loyalsoldier/geoip/plugin/special"
-	_ "github.com/Loyalsoldier/geoip/plugin/v2ray"
+	_ "github.com/vuong2023/geoip/plugin/maxmind"
+	_ "github.com/vuong2023/geoip/plugin/plaintext"
+	_ "github.com/vuong2023/geoip/plugin/special"
+	_ "github.com/vuong2023/geoip/plugin/v2ray"
 )
